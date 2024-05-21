a7d7429b4f86ad6864a47ecea94d3815fc5420f9
Cristian Lopez <analistadesarrollosapfront2_lem@castores.com.mx>
2024-05-21 11:26:23 -0600

    first commit

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { GifsService } from './../services/gifs.service';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
+  styleUrls: ['./busqueda.component.css'],
 })
 export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService){
+
+  }
+
+  public buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.gifsService.buscarGifts(valor);
+
+    this.txtBuscar.nativeElement.value = ' ';
+  }
 }
